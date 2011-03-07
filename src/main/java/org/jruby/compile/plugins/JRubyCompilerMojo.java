849d6acd9b68aac1834c2a7be37ccd03d00f31d6
Pablo Fernandez <fernandezpablo85@gmail.com>
2011-03-06 21:12:34 -0300

    First working version

--- src/main/java/org/jruby/compile/plugins/JRubyCompilerMojo.java
+++ src/main/java/org/jruby/compile/plugins/JRubyCompilerMojo.java
@@ -10,7 +10,7 @@ import org.apache.maven.plugin.*;
  * target.
  * 
  * @goal jruby-compile
- * 
+ * @phase generate-sources
  * @author Pablo Fernandez
  */
 public class JRubyCompilerMojo extends AbstractMojo
@@ -26,15 +26,34 @@ public class JRubyCompilerMojo extends AbstractMojo
   /**
    * Ruby target directory
    * 
-   * @parameter expression = "${basedir}/target/classes"
+   * @parameter expression = "${basedir}/src/main/java"
    * @required
    */
-  protected String rubyTargetDirectory;
+  protected String javaBaseDirectory;
 
   public void execute() throws MojoExecutionException, MojoFailureException
   {
     List<File> rubySources = getRubySources();
-    getLog().info("Compiling " + rubySources.size() + " ruby source files to " + rubyTargetDirectory);
+    getLog().info("Converting " + rubySources.size() + " ruby source files to java in " + javaBaseDirectory);
+    Runtime r = Runtime.getRuntime();
+    try
+    {
+      System.out.println("jrubyc --java " + rubyBaseDirectory + " -t " + javaBaseDirectory);
+      Process p = r.exec("jrubyc --java " + rubyBaseDirectory + " -t " + javaBaseDirectory);
+      int result = p.waitFor();
+      if(result !=0)
+      {
+        getLog().error("jrubyc exited with result code != 0");
+      }
+      else
+      {
+        getLog().info("Ruby compilation successful");
+      }
+    }
+    catch(Exception e)
+    {
+      getLog().error(e.getMessage());
+    }
   }
   
   private List<File> getRubySources()
